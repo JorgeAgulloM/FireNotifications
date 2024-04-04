394a86cf4183464e0a031cc964f1d8fcbcaa8684
Jorge1 <agullojorge@gmail.com>
2024-04-04 22:04:10 +0200

    Created DI

---
+++ app/src/main/java/com/softyorch/firenotifications/di/DataModule.kt
@@ -0,0 +1,19 @@
+package com.softyorch.firenotifications.di
+
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+
+    @Singleton
+    @Provides
+    fun provideFirebaseAnalytics(): FirebaseAnalytics = Firebase.analytics
+}
