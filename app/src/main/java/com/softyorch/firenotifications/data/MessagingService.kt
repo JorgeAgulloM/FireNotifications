035c3a8c72f2483c4d10f87cfa36443eab012997
Jorge1 <agullojorge@gmail.com>
2024-04-04 07:40:46 +0200

    Getting message with app on top

--- app/src/main/java/com/softyorch/firenotifications/data/MessagingService.kt
+++ app/src/main/java/com/softyorch/firenotifications/data/MessagingService.kt
@@ -2,6 +2,7 @@ package com.softyorch.firenotifications.data
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import javax.inject.Inject
 
 class MessagingService @Inject constructor(): FirebaseMessagingService() {
@@ -13,6 +14,18 @@ class MessagingService @Inject constructor(): FirebaseMessagingService() {
 
     private fun sendRegistrationToken(token: String) {
         Log.i("LOGTAG", "Firebase token: $token")
+    }
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+
+        if (message.data.isNotEmpty()) {
+            message.data.forEach { key, value ->
+                Log.i("LOGTAG", "Para la clave $key el valor $value")
+            }
+        } else {
+            Log.w("LOGTAG", "Message: No hay mensages")
+        }
     }
 
 }
