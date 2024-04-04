f9fe0536f8897fbb9ba653c938cd0ebed954ecee
Jorge1 <agullojorge@gmail.com>
2024-04-04 08:10:13 +0200

    Deleted unused imports

--- app/src/main/java/com/softyorch/firenotifications/data/MessagingService.kt
+++ app/src/main/java/com/softyorch/firenotifications/data/MessagingService.kt
@@ -5,9 +5,7 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.content.res.Resources.Theme
 import android.media.RingtoneManager
-import android.media.audiofx.DynamicsProcessing.Config.Builder
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
