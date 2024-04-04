4720a6eab55bca7accf548b0ce9b194901195dc3
Jorge1 <agullojorge@gmail.com>
2024-04-04 07:45:08 +0200

    Getting message when open app from push notification

--- app/src/main/java/com/softyorch/firenotifications/MainActivity.kt
+++ app/src/main/java/com/softyorch/firenotifications/MainActivity.kt
@@ -2,6 +2,7 @@ package com.softyorch.firenotifications
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +10,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val extras = intent.extras
+        if (extras != null) {
+            val test = extras.getString("test")
+            Log.i("LOGTAG", "Para la clave test el valor es $test")
+
+            val example1 = extras.getString("example1")
+            Log.i("LOGTAG", "Para la clave example1 el valor es $example1")
+
+        }
+
     }
 }
