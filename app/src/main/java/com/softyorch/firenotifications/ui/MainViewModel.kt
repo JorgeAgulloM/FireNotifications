755e002c139721a3d7dedf3c1307d89e08375950
Jorge1 <agullojorge@gmail.com>
2024-04-13 21:00:02 +0200

    Implemented subscription function

--- app/src/main/java/com/softyorch/firenotifications/ui/MainViewModel.kt
+++ app/src/main/java/com/softyorch/firenotifications/ui/MainViewModel.kt
@@ -2,12 +2,16 @@ package com.softyorch.firenotifications.ui
 
 import androidx.lifecycle.ViewModel
 import com.softyorch.firenotifications.data.AnalyticsManager
+import com.softyorch.firenotifications.data.TopicManager
 import com.softyorch.firenotifications.data.model.AnalyticModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val analytics: AnalyticsManager): ViewModel() {
+class MainViewModel @Inject constructor(
+    private val analytics: AnalyticsManager,
+    private val topicManager: TopicManager
+): ViewModel() {
 
     fun onLoginSelectedExample() {
         // All my logic
@@ -18,6 +22,10 @@ class MainViewModel @Inject constructor(private val analytics: AnalyticsManager)
     fun customEvent() {
         val analyticModel = AnalyticModel(title = "New custom event", analyticsString = listOf(Pair("CustomString", "String")))
         analytics.sendEvent(analyticModel)
+    }
+
+    fun subscribeToTopic(topic: String) {
+        topicManager.subscribeToTopic(topic)
     }
 
 }
